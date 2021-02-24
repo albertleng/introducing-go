b478684d390d25bad1c8c139e5c59decefb7eaf7
albert <albert.lengcs@stengg.com>
2021-02-24 21:53:11 +0800

    Added Chapter04_control.go

---
+++ Chapter04_Control_Structures/Chapter04_Exercises.md
@@ -0,0 +1,22 @@
+1. What does the following program print?  
+```go
+i := 10
+if i > 10 {
+	fmt.Println("Big")rap
+} else {
+	fmt.Println("Small")
+}
+```
+
+*My answer*: Small
+
+**Book's answer**:   
+
+2. Write a program that prints out all the numbers between 1 and 100 that are evenly divisible by 3 (i.e., 3, 6, 9, etc).
+*My answer*: refer to [Chapter04_control.go](Chapter04_control.go)  
+   
+
+   
+3. Write a program that prints the numbers from 1 to 100, but for multiples of three, print "Fizz" instead of the number, and for the multiple of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".  
+   *My answer*: refer to [Chapter04_control.go](Chapter04_control.go)  
+
--- Chapter04_Control_Structures/Chapter04_control.go
+++ Chapter04_Control_Structures/Chapter04_control.go
@@ -17,20 +17,40 @@ func main() {
 	//	}
 	//}
 
-	for i := 1; i <= 10; i++ {
-		switch i {
-		case 0: fmt.Println("zero")
-		case 1: fmt.Println("one")
-		case 2: fmt.Println("two")
-		case 3: fmt.Println("three")
-		case 4: fmt.Println("four")
-		case 5: fmt.Println("five")
-		case 6: fmt.Println("six")
-		case 7: fmt.Println("seven")
-		case 8: fmt.Println("eight")
-		case 9: fmt.Println("nine")
-		case 10: fmt.Println("ten")
-		default: fmt.Println("Unknown number")
+	//for i := 1; i <= 10; i++ {
+	//	switch i {
+	//	case 0: fmt.Println("zero")
+	//	case 1: fmt.Println("one")
+	//	case 2: fmt.Println("two")
+	//	case 3: fmt.Println("three")
+	//	case 4: fmt.Println("four")
+	//	case 5: fmt.Println("five")
+	//	case 6: fmt.Println("six")
+	//	case 7: fmt.Println("seven")
+	//	case 8: fmt.Println("eight")
+	//	case 9: fmt.Println("nine")
+	//	case 10: fmt.Println("ten")
+	//	default: fmt.Println("Unknown number")
+	//	}
+	//}
+
+	// Exercise 2.
+	//for i := 1; i <= 100; i++ {
+	//	if i % 3 == 0 {
+	//		fmt.Println(i)
+	//	}
+	//}
+
+	// Exercise 3.
+	for i := 1; i <= 100; i++ {
+		if i % 3 == 0 && i % 5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if i % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
 		}
 	}
 }
