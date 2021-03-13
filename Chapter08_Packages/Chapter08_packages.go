7b79c0cb5b84235e185f0b244e27d05c127d7129
albertleng <aleng001@e.ntu.edu.sg>
2021-03-13 16:37:27 +0800

    Adde codes to Chapter08

--- Chapter08_Packages/Chapter08_packages.go
+++ Chapter08_Packages/Chapter08_packages.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"hash/crc32"
 )
 
 func main() {
@@ -32,31 +32,133 @@ func main() {
 
 	//os
 
-	file, err := os.Open("test.txt")
-	if err != nil {
-		// handle the error here
-		return
-	}
-	defer file.Close()
+	//file, err := os.Open("test.txt")
+	//if err != nil {
+	//	// handle the error here
+	//	return
+	//}
+	//defer file.Close()
+	//
+	//// get the file size
+	//stat, err := file.Stat()
+	//if err != nil {
+	//	return
+	//}
+	//
+	//// read the file
+	//bs := make([]byte, stat.Size())
+	//_, err = file.Read(bs)
+	//if err != nil {
+	//	return
+	//}
+	//
+	//str := string(bs)
+	//fmt.Println(str)
+	
+	
+	//bs, err := ioutil.ReadFile("test.txt")
+	//if err != nil {
+	//	return
+	//}
+	//str := string(bs)
+	//fmt.Println(str)
 
-	// get the file size
-	stat, err := file.Stat()
-	if err != nil {
-		return
-	}
 
-	// read the file
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
-
-	str := string(bs)
-	fmt.Println(str)
+	//file, err := os.Create("test.txt")
+	//if err != nil {
+	//	return
+	//}
+	//defer file.Close()
+	//file.WriteString("test")
 
 
+	//dir, err := os.Open(".")
+	//if err != nil {
+	//	return
+	//}
+	//defer dir.Close()
+	//
+	//fileInfos, err := dir.Readdir(-1)
+	//if err != nil {
+	//	return
+	//}
+	//for _, fi := range fileInfos {
+	//	fmt.Println(fi.Name())
+	//}
 
 
+	//filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+	//	fmt.Println(path, info.Size(), err)
+	//	return nil
+	//})
 
+
+	//err := errors.New("error message")
+	//fmt.Println(err)
+
+
+//	Containers and Sort
+
+//	LIST
+//	var x list.List
+//	x.PushBack(1)
+//	x.PushBack(2)
+//	x.PushBack(3)
+//
+//	for e:= x.Front(); e != nil; e=e.Next() {
+//		fmt.Println(e.Value.(int))
+//	}
+
+	//SORT
+	//kids := []Person {
+	//	{"Jill", 9},
+	//	{"Jack", 10},
+	//}
+	//sort.Sort(ByName(kids))
+	//fmt.Println(kids)
+	//
+	//sort.Sort(ByAge(kids))
+	//fmt.Println(kids)
+
+
+	//Hashes and Cryptography
+//	Non-cryptography
+
+	// create a hasher
+	h := crc32.NewIEEE()
+	// write our data to it
+	h.Write([]byte("test"))
+	// calculate the crc32 checksum
+	v := h.Sum32()
+	fmt.Println(v)
+
+}
+type Person struct {
+	Name string
+	Age int
+}
+
+type ByName []Person
+
+func (ps ByName) Len() int {
+	return len(ps)
+}
+
+func (ps ByName) Less(i, j int) bool {
+	return ps[i].Name < ps[j].Name
+}
+
+func (ps ByName) Swap(i, j int) {
+	ps[i], ps[j] = ps[j], ps[i]
+}
+
+type ByAge []Person
+func (this ByAge) Len() int {
+	return len(this)
+}
+func (this ByAge) Less(i, j int) bool {
+	return this[i].Age < this[j].Age
+}
+func (this ByAge) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
 }
